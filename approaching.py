53d3307afa6e2127c8c2578bc51a22d1f77658da
Unknown <joshuanits@gmail.com>
2018-06-05 19:43:53 +0930

    Add approaching, ignore data.csv

---
+++ approaching.py
@@ -0,0 +1,21 @@
+# Simulates an object approaching a planet in 1-dimension, pretends G and M are 1 because they are constant so who cares
+import csv
+
+distance = 100 #Initial distance
+velocity = 0 #Intial velocity
+
+timestep = 0.01 #Number of seconds use for each step
+totaltime = 0
+
+with open('data.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile, delimiter=' ')
+    writer.writerow(["Time", "Distance", "Velocity", "Acceleration"])
+    writer.writerow([totaltime, distance, velocity, 0])
+    while(distance > 0):
+        acceleration = 1 / (distance ** 2)
+        velocity += acceleration * timestep
+        distance -= velocity * timestep
+        totaltime += timestep
+        writer.writerow([totaltime, distance, velocity, acceleration])
+
+print("Reached planet in " + str(totaltime) + "s, " + str(totaltime/timestep) + " steps.")
